39986914b11b6e437e17fd69e6755d8f81843730
MatheuzFelix <matheuzfelixamorim@gmail.com>
2022-08-07 19:48:56 -0300

    Web_scraping_selenium

--- Setup.py
+++ Setup.py
@@ -1,12 +1,10 @@
-from itertools import product
-from urllib import response
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from tkinter import messagebox
 from selenium import webdriver
-import requests
-import scrapy
+
 import time
 
 
@@ -24,22 +22,36 @@ class Consulta:
 
         # elementos do site da Magazine
         self.inputBusca = By.ID, 'input-search'
-        self.produtos = By.XPATH, "*//div[@class='sc-iyyVIK gdPMEf']"
+        self.produtos = By.XPATH, "*//div[@class='sc-iyyVIK gdPMEf']" # Xpath criado com a biblioteca scrapy para acessa todos os preços encontrados na pesquisa 
+        self.VlrdoProduto = By.XPATH, "*//div[@class='sc-hKwDye hBjQcp sc-JEhMO epMUid']" # Xpath criado com a biblioteca scrapy para ter acesso a todos os produtos do site
 
     def magazineLuiza (self):
         driver.get('https://www.magazineluiza.com.br')
         while True:
             try:
-                driver.find_element(*self.inputBusca).send_keys('Sansung a23')
+                driver.find_element(*self.inputBusca).send_keys('Cadeira gamer')
                 driver.find_element(*self.inputBusca).send_keys(Keys.ENTER)
                 break
             except: time.sleep(1)
         time.sleep(10)
-        url = driver.current_url
-        response = requests.get(url)
-        print(response)
-        produtos = driver.find_elements(*self.produtos)[1].text
-        print(produtos)
+        QuantProdutos = len(driver.find_elements(*self.VlrdoProduto))
+        listaValores = {
+        }
+        for i in range (QuantProdutos):
+            vlr = driver.find_elements(*self.VlrdoProduto)[i].text
+            comparador = vlr[3:8]
+            comparador = comparador.replace('.','')
+            comparador = comparador.split(",")
+            comparador = comparador[0]
+            dicionary = {
+                i:comparador,
+            }
+            listaValores.update(dicionary)
+        minimo = min(listaValores, key= listaValores.get)
+
+        barato = driver.find_elements(*self.produtos)[minimo].text
+        messagebox.showinfo('Produto mais barato encontrado',\
+            barato)
 
 executar = Consulta(driver)
 
